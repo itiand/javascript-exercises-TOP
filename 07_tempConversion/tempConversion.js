b95e7519a044ef82ae24317fc4e8e1ddcd51a7e2
Christian Delos Reyes <Christian.dreyes@gmail.com>
2023-04-24 12:19:04 -0400

    my attempt

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  // Celsius = (fahrenheit - 32) * 5/9
+    //convert to one decical, use Math.round() --> RoundedCelsius = Math.round(Celsius * 10)/10
+  let celsius = (fahrenheit - 32) * 5/9;
+  return Math.round(celsius * 10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  // Fahrenheit =  celsius * 9/5 + 32
+  const fahrenheit = celsius * 9/5 + 32
+  return Math.round(fahrenheit*10)/10;
 };
 
 // Do not edit below this line
