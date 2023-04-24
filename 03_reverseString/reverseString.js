da4089c31ad708615cd2bc800c9f3ce898066ed3
Christian Delos Reyes <Christian.dreyes@gmail.com>
2023-04-24 11:56:19 -0400

    my attempts to these exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+   let string = str.split('');
+   let reverse = string.reverse().join('')
+   return reverse
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+   let accumulated = 0;
+   if (start < 0 || end < 0 || isNaN(start) || isNaN(end) || typeof start !== 'number' || typeof end !== 'number') {
+      return 'ERROR'
+   } else {
+      if (start < end) {
+         for(let i = start; i <= end; i ++) {
+            accumulated += i;
+         }
+      } else if (start > end) {
+         for(let i = start; i >= end; i--) {
+            accumulated += i
+         }
+      }
+   }
+   return accumulated;
 };
 
 // Do not edit below this line
