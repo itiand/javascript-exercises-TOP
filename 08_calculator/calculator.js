7d4c16b1f0bb8cc2d82978b1ad811e99bb80b992
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-05-23 17:18:46 -0400

    finish calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,57 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let acc = 0
+	for(let i = 0; i < arr.length; i++) {
+    acc += arr[i];
+  }
+  return acc;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let prod;
+  for(let i = 0; i < arr.length; i++) {
+    if(i === 0) {
+      prod = arr[i]
+    } else {
+      prod *= arr[i]
+    }
+  }
+  return prod;
 };
 
-const power = function() {
-	
+
+const power = function(a,b) {
+  let output
+	for(let i = 0; i < b; i++) {
+    if(i === 0) {
+      output = a;
+    } else {
+      output *= a
+    }
+  }
+  return output
 };
 
-const factorial = function() {
-	
+
+const factorial = function(num) {
+  if(num === 0) {
+    return 1;
+  }
+
+  let result = num;
+  for(let i = 1; i < num; i++) {
+    result *= (num-i)
+  }
+	return result;
 };
+
 
 // Do not edit below this line
 module.exports = {
