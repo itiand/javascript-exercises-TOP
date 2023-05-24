888613ec059a9dd1067e6556fe431da5356af2c8
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-05-23 21:03:40 -0400

    complete palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
 
+const palindromes = function (str) {
+  let convertStr = str.toLowerCase().replace(/[^a-z]/g, "");
+  let convertStr1 = convertStr.split('').reverse().join('');
+  return convertStr1 === convertStr
 };
+
+console.log(palindromes('A car, a man, a maraca'));
 
 // Do not edit below this line
 module.exports = palindromes;
