47badb8b4aeeedcac73deca8784a54ea8b6bbd07
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-05-24 10:44:39 -0400

    getTheTitles function finish

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,21 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(bookArr) {
+  let bookTitles = []
+  for(let i = 0; i < bookArr.length; i++) {
+    bookTitles.push(bookArr[i]['title'])
+  }
+  return bookTitles
 };
+
+console.log(getTheTitles([
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+]))
 
 // Do not edit below this line
 module.exports = getTheTitles;
