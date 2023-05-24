b3525b8cd2cb3840b08e5bb22121aaa2f236b325
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-05-24 10:33:29 -0400

    finish fibb sequence function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(pos) {
+  if(pos < 0) {
+    return "OOPS"
+  }
+  let seq = [0,1]
+  for(let i = 1; i <= pos; i++) {
+    if(i >= 2) {
+      seq.push(seq[i-1]+ seq[i - 2]);
+    }
+  }
+  return seq[pos]
 };
 
 // Do not edit below this line
